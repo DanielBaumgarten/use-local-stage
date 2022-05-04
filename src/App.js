ee52abca3955b9ef9962c6ae1d75cc41819e8b1c
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2022-05-04 18:07:15 -0300

    limpando arquivos para iniciar novo projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useLocalStorage } from 'react-use';
 
 function App() {
+
+  const [value, setValue, remove] = useLocalStorage('my-key', {"name": 'Daniel', "age": 30 });
+  const [value2, setValue2, remove2] = useLocalStorage('my-key-2', 'valor incial-daniel-lopes');
+  const [value3, setValue3, remove3] = useLocalStorage('my-key-3', 'valor-inicial-3', {
+    raw: true
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Hello World</h1>
+      <div>        
+        <div> Value: {value.name}</div>
+        <button onClick={() => setValue('bar')}>bar</button>
+        <button onClick={() => setValue('baz')}>baz</button>
+        <button onClick={() => remove()}>Remove</button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
